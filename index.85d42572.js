4c5c6de5764bc2fc44d7e7c16680cb218f3ec6ac
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-12-04 20:57:08 +0200

    make build

---
+++ index.85d42572.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),o=t.offsetWidth,f=t.offsetHeight,c=e.offsetWidth,l=e.offsetHeight;e.style.top="".concat((f-l)/2,"px"),e.style.left="".concat((o-c)/2,"px");
+//# sourceMappingURL=index.85d42572.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const topPosition = (wallHeight - spiderHeight) / 2;
+const leftPosition = (wallWidth - spiderWidth) / 2;
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
